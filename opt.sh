94a0ff33e4de88f03f7ea1ad24842d07113895ce
Zhuoyan Xu <zxu68341@icloud.com>
2023-10-31 11:58:33 -0500

    opt for simulation

---
+++ opt.sh
@@ -0,0 +1,5 @@
+python tools/opt.py \
+        --matrix_data "./dataset/sim_matrix_data.csv" \
+        --study_names "./dataset/sim_study_name.csv" \
+        --output_path "output/sim" \
+        --rank 20
