46d7b3dd71e51f6190d2c1562e2ae90845e2cb75
ysc12451 <ysc12451@163.com>
2023-11-10 08:49:42 -0600

    update visz for noise ratio

--- notebook/run_multiple_opt.sh
+++ notebook/run_multiple_opt.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_1/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_1/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_1/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_2/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_2/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_2/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_3/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_3/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_3/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_4/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_4/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_4/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_5/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_5/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_5/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_6/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_6/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_6/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_7/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_7/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_7/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_8/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_8/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_8/output" --rank 20
-python opt.py --matrix_data "./diff_prop_sim_smallnoise/avg_prop_9/sim_matrix_data.csv" --study_names "./diff_prop_sim_smallnoise/avg_prop_9/sim_study_name.csv" --output_path "./diff_prop_sim_smallnoise/avg_prop_9/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_1/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_1/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_1/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_2/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_2/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_2/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_3/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_3/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_3/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_4/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_4/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_4/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_5/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_5/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_5/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_6/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_6/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_6/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_7/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_7/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_7/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_8/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_8/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_8/output" --rank 20
+# python opt.py --matrix_data "./diff_prop_sim/avg_prop_9/sim_matrix_data.csv" --study_names "./diff_prop_sim/avg_prop_9/sim_study_name.csv" --output_path "./diff_prop_sim/avg_prop_9/output" --rank 20
+
+
+python opt.py --matrix_data "./diff_noise/noise_0/sim_matrix_data.csv" --study_names "./diff_noise/noise_0/sim_study_name.csv" --output_path "./diff_noise/noise_0/output" --rank 20
+python opt.py --matrix_data "./diff_noise/noise_0.1/sim_matrix_data.csv" --study_names "./diff_noise/noise_0.1/sim_study_name.csv" --output_path "./diff_noise/noise_0.1/output" --rank 20
+python opt.py --matrix_data "./diff_noise/noise_0.01/sim_matrix_data.csv" --study_names "./diff_noise/noise_0.01/sim_study_name.csv" --output_path "./diff_noise/noise_0.01/output" --rank 20
+python opt.py --matrix_data "./diff_noise/noise_0.001/sim_matrix_data.csv" --study_names "./diff_noise/noise_0.001/sim_study_name.csv" --output_path "./diff_noise/noise_0.001/output" --rank 20
+python opt.py --matrix_data "./diff_noise/noise_1.0/sim_matrix_data.csv" --study_names "./diff_noise/noise_1.0/sim_study_name.csv" --output_path "./diff_noise/noise_1.0/output" --rank 20
